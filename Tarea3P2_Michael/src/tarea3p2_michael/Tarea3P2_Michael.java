78cc740ae2133f77c11a4e821b8f8c6b729468cb
michael-martinez14 <michaelmartinez2083@unitec.edu>
2024-01-22 23:00:52 -0600

    Fin

---
+++ Tarea3P2_Michael/src/tarea3p2_michael/Tarea3P2_Michael.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea3p2_michael;
+
+/**
+ *
+ * @author Michael Martínez
+ */
+public class Tarea3P2_Michael {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
